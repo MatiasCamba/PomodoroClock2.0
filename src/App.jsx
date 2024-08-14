6df60f3c7d8f894af90fac71ccfffcbac0c6fa61
matiascamba <matiezequielcamba@gmail.com>
2024-08-14 17:20:33 -0300

    Initialize Pomodoro Repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center bg-yellow-300 font-bold text-3xl text-black">
+       Pomodoro Clock
+      </h1>
+    </>
+  );
+}
+
+export default App;
