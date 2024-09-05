1c79c315050b490bdbf130ce2bc867649fa9e27a
matiascamba <matiezequielcamba@gmail.com>
2024-09-05 19:51:27 -0300

    pwa try7

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,27 +10,7 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
-      manifest: {
-        name: "Nombre de tu App",
-        short_name: "App",
-        description: "Descripción de tu App",
-        theme_color: "#ffffff",
-        icons: [],
-        screenshots: [
-          {
-            src: "/assets/github.png",
-            sizes: "540x720",
-            type: "image/png",
-            form_factor: "narrow",
-          },
-          {
-            src: "/assets/linkedin.png",
-            sizes: "1080x1920",
-            type: "image/png",
-            form_factor: "standard",
-          },
-        ],
-      },
+      manifest,
     }),
   ],
 });
