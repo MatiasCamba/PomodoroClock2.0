73724cab28f7024ce040d38c60946bedcb0664c2
matiascamba <matiezequielcamba@gmail.com>
2024-09-05 19:48:01 -0300

    pwa try6

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
       manifest: {
-        name: 'Nombre de tu App',
-        short_name: 'App',
-        description: 'Descripción de tu App',
-        theme_color: '#ffffff',
-        icons: [
+        name: "Nombre de tu App",
+        short_name: "App",
+        description: "Descripción de tu App",
+        theme_color: "#ffffff",
+        icons: [],
+        screenshots: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "/assets/github.png",
+            sizes: "540x720",
+            type: "image/png",
+            form_factor: "narrow",
           },
           {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+            src: "/assets/linkedin.png",
+            sizes: "1080x1920",
+            type: "image/png",
+            form_factor: "standard",
+          },
+        ],
+      },
+    }),
   ],
-  
-})
+});
