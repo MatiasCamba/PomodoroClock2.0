21df5be1b253317b5ae13f733554f64178876176
matiascamba <matiezequielcamba@gmail.com>
2024-09-05 19:25:41 -0300

    pwa try2

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('ServiceWorker registrado con éxito:', registration);
+    }).catch(error => {
+      console.log('Error en el registro del ServiceWorker:', error);
+    });
+  });
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
