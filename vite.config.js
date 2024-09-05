732a692b4ccfc398fc453460331ee7d3aefe1276
matiascamba <matiezequielcamba@gmail.com>
2024-09-05 19:41:39 -0300

    pwa try4

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,18 @@ export default defineConfig({
         short_name: 'App',
         description: 'Descripción de tu App',
         theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ],
