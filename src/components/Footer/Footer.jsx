b002fa432aa36b1a3189a06f317e74fdf839b045
matiascamba <matiezequielcamba@gmail.com>
2024-09-30 11:07:42 -0300

    (/components/footer):agrege comentario

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="flex flex-1 align-middle items-center justify-center gap-8 inset-x-0 bottom-0 opacity-70">
       <span>
-        © Matias Camba<sup>®</sup>
+        © Matias Camba gracias<sup>®</sup>
       </span>
       <a href="https://github.com/MatiasCamba" target="_blank">
         <img src={github} alt="GitHub" className="w-20 h-20" />
